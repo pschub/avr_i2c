b2b09209db36944c9c38c8e9bfc9f7ceb4410ccb
pschub <8bitbicycle@gmail.com>
2013-10-14 19:25:09 -0700

    Added license and readme for public use.

---
+++ README.md
@@ -0,0 +1,7 @@
+avr_i2c
+=======
+
+Demo of i2c capabilities on atmega328
+
+Reads four bytes from an EEPROM with i2c, then transmits these
+four bytes over UART.
--- i2c.h
+++ i2c.h
@@ -3,12 +3,27 @@
    i2c Helper
    Patrick Schubert
    Created March 30, 2013
-
+   
    Currently implementing just initialization and single byte read/writes.
    Nice things to have in the future:
    - Interrupt-driven
    - Circular buffers
    - Ice cream maker
+
+Copyright (C) 2013 Patrick Schubert 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef I2CHELPER_H
